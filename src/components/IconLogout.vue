071c523d5880407bf9c99372e62f190401489c76
Ari <arisenger0@gmail.com>
2024-05-03 17:17:13 +0200

    add: icon-logout component

---
+++ src/components/IconLogout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex justify-center items-center text-voloblue-200 text-xl border border-l-voloblue-200 border-l-4 bg-vologray-100 py-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+    <path
+      fill="currentColor"
+      d="M189.66 122.34a8 8 0 0 1 0 11.32l-72 72a8 8 0 0 1-11.32-11.32L164.69 136H32a8 8 0 0 1 0-16h132.69l-58.35-58.34a8 8 0 0 1 11.32-11.32ZM216 32a8 8 0 0 0-8 8v176a8 8 0 0 0 16 0V40a8 8 0 0 0-8-8"
+    ></path>
+  </svg>
+    </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'PhArrowLineRight'
+}
+</script>
