5ee6c9714d0590f0ea6e07208c6302a8bdcad173
Ari <arisenger0@gmail.com>
2024-05-06 17:15:00 +0200

    fix: move NavigationBar to root component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <RouterView />
+  <div class="h-screen overflow-hidden grid grid-cols-[70px,auto] divide-x">
+    <NavigationBar>
+      <template v-slot:nav-button>
+        <IconLogout />
+      </template>
+    </NavigationBar>
+    <RouterView />
+  </div>
 </template>
 
-<script></script>
+<script>
+import NavigationBar from '@/components/NavigationBar.vue'
+import IconLogout from '@/components/icon-components/IconLogout.vue'
+
+export default {
+  components: { NavigationBar, IconLogout }
+}
+</script>
+
+<!-- max-w-screen-2xl  -->
