c999de2e14fc9cd5a7638388d6d699c8050a5f06
Ari <arisenger0@gmail.com>
2024-05-10 19:05:04 +0200

    add: tableComponent in volunteersView

--- src/views/VolunteersView.vue
+++ src/views/VolunteersView.vue
@@ -1,24 +1,29 @@
 <template>
-  <main class="flex justify-between bg-vologray-100 pt-6 px-8">
+  <main class=" bg-vologray-100">
+    <div class="flex justify-between pt-6 px-8">
     <div class="flex justify-between w-96 h-11 bg-white px-4 py-2 border rounded-md">
-    <input
-      class="outline-none placeholder-vologray-300"
-      type="text"
-      placeholder="Suche nach Freiwilligen"
-    />
-    <IconSearch />
+      <input
+        class="outline-none placeholder-vologray-300"
+        type="text"
+        placeholder="Suche nach Freiwilligen"
+      />
+      <IconSearch />
+    </div>
+    <BaseButton>Freiwillige:n anlegen</BaseButton>   
   </div>
-  <BaseButton>Freiwillige:n anlegen</BaseButton>
+   <TableComponent class="w-full mx-8 mt-12" /> 
   </main>
+  
   
 </template>
 <script>
-import BaseButton from '@/components/BaseButton.vue';
-import IconSearch from '../components/IconSearch.vue';
+import BaseButton from '@/components/BaseButton.vue'
+import IconSearch from '../components/IconSearch.vue'
+import TableComponent from '../components/TableComponent.vue'
 
 export default {
   name: 'VolunteersView',
-  components:  {IconSearch, BaseButton}
+  components: { IconSearch, BaseButton, TableComponent }
 }
 </script>
 <style></style>
