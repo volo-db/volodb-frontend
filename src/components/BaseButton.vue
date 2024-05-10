ad53b93b410903b147c219a84a2a8e77f42358f7
Ari <arisenger0@gmail.com>
2024-05-10 08:43:16 +0200

    fix: remove margin and width

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="border rounded-lg text-white text-sm bg-voloblue-200 w-28 h-11 mt-20">
+  <button class="border rounded-lg text-white text-sm bg-voloblue-200 px-6 h-11">
     <slot></slot>
   </button>
 </template>
@@ -7,3 +7,6 @@
 <script>
 export default {}
 </script>
+
+
+<!-- border rounded-lg text-white text-sm bg-voloblue-200 w-28 h-11 mt-20 -->
