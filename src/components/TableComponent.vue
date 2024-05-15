39bf0f046d6b37d2aa075f4e60f0379f3f9e0896
Ari <arisenger0@gmail.com>
2024-05-15 12:43:14 +0200

    fix: padding

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,49 +1,50 @@
 <template> 
-<div>
-    <table class="w-full" v-if="volunteerStore.volunteers.length > 0" >
+<div class="overflow-x-auto" v-bind="$attrs">
+    <table class="w-full " v-if="volunteerStore.volunteers.length > 0" >
       <thead>
         <tr>
-          <td class="pr-8 pb-3 pl-3 text-vologray-700 text-sm">
+          <td class="pb-3 pl-4  text-vologray-700 text-sm">
             Name<img :src="listSortArrows" class="pl-2 inline" />
           </td>
-          <td class="pr-8 pb-3 text-vologray-700 text-sm">
+          <td class="pb-3 text-vologray-700 text-sm">
             Vorname<img :src="listSortArrows" class="pl-2 inline" />
           </td>
-          <td class="pr-8 pb-3 text-vologray-700 text-sm">
+          <td class="pb-3 text-vologray-700 text-sm">
             Einsatzstelle<img :src="listSortArrows" class="pl-2 inline" />
           </td>
-          <td class="pr-8 pb-3 text-vologray-700 text-sm">
+          <td class="pb-3 text-vologray-700 text-sm">
             Jahrgang<img :src="listSortArrows" class="pl-2 inline" />
           </td>
-          <td class="pr-8 pb-3 text-vologray-700 text-sm">
+          <td class="pb-3 text-vologray-700 text-sm">
             Unterlagen<img :src="listSortArrows" class="pl-2 inline" />
           </td>
-          <td class="pr-8 pb-3 text-vologray-700 text-sm">
+          <td class="pb-3 text-vologray-700 text-sm">
             gebuchte Seminare<img :src="listSortArrows" class="pl-2 inline" />
           </td>
         </tr>
       </thead>
-      <tbody class="bg-white outline outline-white rounded">
+      <tbody class="bg-white outline outline-white rounded ">
         <tr
-          class="border-b h-11"
+          class="border-b h-14  "
           v-for="volunteer of volunteerStore.volunteers"
           :key="volunteer.id"
         >
-          <td class="pr-8 pl-3 font-bold">{{ volunteer.person.lastname }}</td>
-          <td class="pr-8 font-bold">{{ volunteer.person.firstname }}</td>
-          <td class="pr-8">{{ volunteer.birthplace }}</td>
-          <td class="pr-8">2023/24</td>
-          <td class="pr-8">2/5</td>
-          <td class="pr-8">25/25</td>
+          <td class="pl-4 font-bold">{{ volunteer.person.lastname }}</td>
+          <td class="font-bold">{{ volunteer.person.firstname }}</td>
+          <td>{{ volunteer.birthplace }}</td>
+          <td>2023/24</td>
+          <td>2/5</td>
+          <td>25/25</td>
           <td class="text-voloblue-100"><IconDetailViewArrow /></td>
-        </tr>
-        <td class="bg-white text-voloblue-200 text-sm text-center" colspan="7">1/5</td>
+        </tr> 
+        <td class="bg-white text-voloblue-200 text-sm text-center" colspan="7">1/5</td> 
       </tbody>
     </table>
+  </div>
     <ModalContainer v-if="volunteerStore.fetching">
       <div class="p-4 flex flex-row gap-2 items-center text-md"><icon-spinner />loading ...</div>
     </ModalContainer>
-  </div>
+  
 </template>
 
 <script>
