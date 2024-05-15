cd24c601d757b951fc27c15a8361c7aca24fceb6
Ari <arisenger0@gmail.com>
2024-05-15 09:54:28 +0200

    add: modal for loadingscreen, data input from fetching

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,51 +1,56 @@
 <template>
-<slot></slot>
-   <h1>Table:</h1>
-   <div v-if="volunteerStore.volunteers.length > 0">
-    <!-- <p>{{ volunteerStore.volunteers[0].person.firstname}}</p> -->
+  <div>
+  <table class="w-full" v-if="volunteerStore.volunteers.length > 0">
     <thead>
-    <tr>
-      <td class="pr-8 pb-3 pl-3 text-gray-500 text-sm">
-        Name<img :src="listSortArrows" class="pl-2 inline" />
-      </td>
-      <td class="pr-8 pb-3 text-gray-500 text-sm">
-        Vorname<img :src="listSortArrows" class="pl-2 inline" />
-      </td>
-      <td class="pr-8 pb-3 text-gray-500 text-sm">
-        Einsatzstelle<img :src="listSortArrows" class="pl-2 inline" />
-      </td>
-      <td class="pr-8 pb-3 text-gray-500 text-sm">
-        Jahrgang<img :src="listSortArrows" class="pl-2 inline" />
-      </td>
-      <td class="pr-8 pb-3 text-gray-500 text-sm">
-        Unterlagen<img :src="listSortArrows" class="pl-2 inline" />
-      </td>
-      <td class="pr-8 pb-3 text-gray-500 text-sm">
-        gebuchte Seminare<img :src="listSortArrows" class="pl-2 inline" />
-      </td>
-    </tr>
-  </thead>
-  <tbody class="bg-white outline outline-white rounded">
-    <tr class="border-b h-11">
-      <td class="pr-8 pl-3 font-bold">Müller</td>
-      <td class="pr-8 font-bold">{{ volunteerStore.volunteers[0].person.firstname}}</td>
-      <td class="pr-8">München</td>
-      <td class="pr-8">2023/24</td>
-      <td class="pr-8">2/5</td>
-      <td class="pr-8">25/25</td>
-      <td class="text-voloblue-100">></td>
-    </tr>
-  </tbody>
-  </div>
+      <tr>
+        <td class="pr-8 pb-3 pl-3 text-gray-500 text-sm">
+          Name<img :src="listSortArrows" class="pl-2 inline" />
+        </td>
+        <td class="pr-8 pb-3 text-gray-500 text-sm">
+          Vorname<img :src="listSortArrows" class="pl-2 inline" />
+        </td>
+        <td class="pr-8 pb-3 text-gray-500 text-sm">
+          Einsatzstelle<img :src="listSortArrows" class="pl-2 inline" />
+        </td>
+        <td class="pr-8 pb-3 text-gray-500 text-sm">
+          Jahrgang<img :src="listSortArrows" class="pl-2 inline" />
+        </td>
+        <td class="pr-8 pb-3 text-gray-500 text-sm">
+          Unterlagen<img :src="listSortArrows" class="pl-2 inline" />
+        </td>
+        <td class="pr-8 pb-3 text-gray-500 text-sm">
+          gebuchte Seminare<img :src="listSortArrows" class="pl-2 inline" />
+        </td>
+      </tr>
+    </thead>
+    <tbody class="bg-white outline outline-white rounded">
+      <tr class="border-b h-11" v-for="volunteer of volunteerStore.volunteers" :key="volunteer.id">
+        <td class="pr-8 pl-3 font-bold">{{ volunteer.person.lastname }}</td>
+        <td class="pr-8 font-bold">{{ volunteer.person.firstname }}</td>
+        <td class="pr-8">{{ volunteer.birthplace }}</td>
+        <td class="pr-8">2023/24</td>
+        <td class="pr-8">2/5</td>
+        <td class="pr-8">25/25</td>
+        <td class="text-voloblue-100"><IconDetailViewArrow /></td>
+      </tr>
+      <td class="bg-white text-voloblue-200 text-sm text-center" colspan="7">1/5</td>
+    </tbody>
+  </table>
+  <ModalContainer v-if="volunteerStore.fetching">
+    <div class="p-4 flex flex-row gap-2 items-center text-md"><icon-spinner />loading ...</div>
+  </ModalContainer>
+</div>
 </template>
 
 <script>
 import listSortArrows from '../assets/logos/list-sort-arrows.svg'
-// import IconDetailViewArrow from './IconDetailViewArrow.vue'
+import IconDetailViewArrow from './IconDetailViewArrow.vue'
 import { useVolunteerStore } from '@/stores/VolunteerStore'
+import ModalContainer from '@/components/ModalContainer.vue'
+import IconSpinner from '@/components/IconSpinner.vue'
 
 export default {
-  // components: { IconDetailViewArrow },
+  components: { IconDetailViewArrow, ModalContainer, IconSpinner },
   setup: () => {
     const volunteerStore = useVolunteerStore()
     return { volunteerStore }
@@ -56,16 +61,14 @@ export default {
     }
   },
   created() {
-  this.volunteerStore.getVolunteers().catch(error => {
-    console.error('Error fetching volunteers:', error);
-  });
-  console.log('created');
-  console.log(this.volunteerStore.volunteers)
-},
-
+    this.volunteerStore.getVolunteers().catch((error) => {
+      console.error('Error fetching volunteers:', error)
+    })
+    console.log('created')
+    console.log(this.volunteerStore.volunteers)
+  }
 }
 </script>
-
 
 <!-- <table>
   <thead>
