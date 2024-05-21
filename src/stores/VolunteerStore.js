e904668c3fe5f1b0a4fae5e940744cbd0124bf7e
Ari <arisenger0@gmail.com>
2024-05-21 13:41:33 +0200

    delete: comments

--- src/stores/VolunteerStore.js
+++ src/stores/VolunteerStore.js
@@ -34,7 +34,6 @@ export const useVolunteerStore = defineStore('volunteerStore', {
       await fetch(`${import.meta.env.VITE_BASE_URL}/volunteers?page=${pageNumber}`, {
         method: 'GET',
         headers: {
-          // 'content-type': 'application/json',
           authorization: `Bearer ${this.token}`
         }
       })
@@ -54,59 +53,3 @@ export const useVolunteerStore = defineStore('volunteerStore', {
   },
   getters: {}
 })
-
-// import { defineStore } from 'pinia'
-
-// export const useVolunteerStore = defineStore('volunteerStore', {
-//   state: () => {
-//     return {
-//       fetching: false,
-//       token: JSON.parse(localStorage.getItem('user-store')).token,
-//       data: null,
-//       volunteers: null
-//     }
-//   },
-//   actions: {
-//     async saveVolunteer(volunteer) {
-//       // If there's no token, something went wrong
-//       if (!this.token) throw Error('VoloDB-ERROR\n🙅‍♀️ ups! not logged in.')
-
-//       this.fetching = true
-//       await fetch(`${import.meta.env.VITE_BASE_URL}/volunteers`, {
-//         method: 'POST',
-//         headers: {
-//           'content-type': 'application/json',
-//           authorization: `Bearer ${this.token}`
-//         },
-//         body: JSON.stringify(volunteer)
-//       })
-//         .then((res) => {
-//           if (!res.ok) throw Error(`VoloDB-ERROR\n🙅‍♀️ posting new volunteer failed! (${res.status}`)
-//           return res.json()
-//         })
-//         .finally(() => (this.fetching = false))
-//     },
-//     async getVolunteers(pageNumber) {
-//       this.fetching = true
-//       await fetch(`${import.meta.env.VITE_BASE_URL}/volunteers?page=${pageNumber}`, {
-//         method: 'GET',
-//         headers: {
-//           authorization: `Bearer ${this.token}`
-//         }
-//       })
-//         .then((res) => {
-//           console.log(res.status)
-//           if (!res.ok) {
-//             throw Error(`ERROR:${res.status}`)
-//           }
-//           return res.json()
-//         })
-//         .then((data) => {
-//           this.data = data
-//           this.volunteers = data.content
-//         })
-//         .finally(() => (this.fetching = false))
-//     }
-//   },
-//   getters: {}
-// })
