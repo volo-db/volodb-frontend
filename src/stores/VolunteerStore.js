205cba1d531be61448de85802e8356d430c82a81
Fabian Urner <fabian.urner+gh@gmail.com>
2024-06-03 12:10:45 +0200

    add: new api-utils-lib for fetching data from backend

--- src/stores/VolunteerStore.js
+++ src/stores/VolunteerStore.js
@@ -1,60 +1,5 @@
 import { defineStore } from 'pinia'
-
-// private function to use inside "getVolunteer"
-const getContactsOfVolunteer = async (volunteerId, token) => {
-  //clear selected volunteer contacts like phoneNr, email, whatsap, etc...
-
-  // If there's no token, something went wrong
-  if (!token) throw Error('VoloDB-ERROR\n🙅‍♀️ ups! not logged in.')
-
-  let tempContacts = null
-
-  await fetch(`${import.meta.env.VITE_BASE_URL}/volunteers/${volunteerId}/contacts`, {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json',
-      authorization: `Bearer ${token}`
-    }
-  })
-    .then((res) => {
-      if (!res.ok)
-        throw Error(`VoloDB-ERROR\n🙅‍♀️ fetching volunteer contacts failed! (${res.status}`)
-      return res.json()
-    })
-    .then((contacts) => {
-      tempContacts = contacts
-    })
-
-  return tempContacts
-}
-
-// private function to use inside "getVolunteer"
-const getAddressesOfVolunteer = async (volunteerId, token) => {
-  //clear selected volunteer contacts like phoneNr, email, whatsap, etc...
-
-  // If there's no token, something went wrong
-  if (!token) throw Error('VoloDB-ERROR\n🙅‍♀️ ups! not logged in.')
-
-  let tempAddresses = null
-
-  await fetch(`${import.meta.env.VITE_BASE_URL}/volunteers/${volunteerId}/addresses`, {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json',
-      authorization: `Bearer ${token}`
-    }
-  })
-    .then((res) => {
-      if (!res.ok)
-        throw Error(`VoloDB-ERROR\n🙅‍♀️ fetching volunteer addresses failed! (${res.status}`)
-      return res.json()
-    })
-    .then((addresses) => {
-      tempAddresses = addresses
-    })
-
-  return tempAddresses
-}
+import { vdbFetchData } from '@/utils/api'
 
 export const useVolunteerStore = defineStore('volunteerStore', {
   state: () => {
@@ -75,22 +20,8 @@ export const useVolunteerStore = defineStore('volunteerStore', {
       if (!this.token) throw Error('VoloDB-ERROR\n🙅‍♀️ ups! not logged in.')
 
       this.fetching = true
-      await fetch(`${import.meta.env.VITE_BASE_URL}/volunteers`, {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          authorization: `Bearer ${this.token}`
-        },
-        body: JSON.stringify(volunteer)
-      })
-        .then((res) => {
-          if (!res.ok) throw Error(`VoloDB-ERROR\n🙅‍♀️ posting new volunteer failed! (${res.status}`)
-          return res.json()
-        })
-        .then((volunteer) => {
-          this.selectedVolunteer = volunteer
-        })
-        .finally(() => (this.fetching = false))
+      this.selectedVolunteer = await vdbFetchData('volunteers', 'POST', this.token, volunteer)
+      this.fetching = false
     },
     async getVolunteer(volunteerId) {
       //clear selected volunteer
@@ -100,68 +31,50 @@ export const useVolunteerStore = defineStore('volunteerStore', {
       if (!this.token) throw Error('VoloDB-ERROR\n🙅‍♀️ ups! not logged in.')
 
       this.fetching = true
-      await fetch(`${import.meta.env.VITE_BASE_URL}/volunteers/${volunteerId}`, {
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json',
-          authorization: `Bearer ${this.token}`
-        }
-      })
-        .then((res) => {
-          if (!res.ok) throw Error(`VoloDB-ERROR\n🙅‍♀️ fetching volunteer failed! (${res.status}`)
-          return res.json()
-        })
-        .then(async (volunteer) => {
-          this.selectedVolunteer = volunteer
-          this.selectedVolunteerContacts = await getContactsOfVolunteer(volunteerId, this.token)
-          this.selectedVolunteerAddresses = await getAddressesOfVolunteer(volunteerId, this.token)
-        })
-        .finally(() => (this.fetching = false))
+
+      this.selectedVolunteer = await vdbFetchData(`volunteers/${volunteerId}`, 'GET', this.token)
+
+      this.selectedVolunteerContacts = await vdbFetchData(
+        `volunteers/${volunteerId}/contacts`,
+        'GET',
+        this.token
+      )
+
+      this.selectedVolunteerAddresses = await vdbFetchData(
+        `volunteers/${volunteerId}/addresses`,
+        'GET',
+        this.token
+      )
+
+      this.fetching = false
     },
     async getVolunteers(pageNumber = 0) {
+      // If there's no token, something went wrong
+      if (!this.token) throw Error('VoloDB-ERROR\n🙅‍♀️ ups! not logged in.')
+
       this.fetching = true
-      await fetch(
-        `${import.meta.env.VITE_BASE_URL}/volunteers?page=${pageNumber}&sortField=person.lastname&sortOrder=asc`,
-        {
-          method: 'GET',
-          headers: {
-            authorization: `Bearer ${this.token}`
-          }
-        }
+      this.volunteersPage = await vdbFetchData(
+        `volunteers?page=${pageNumber}&sortField=person.lastname&sortOrder=asc`,
+        'GET',
+        this.token
       )
-        .then((res) => {
-          if (!res.ok)
-            throw Error(`VoloDB-ERROR\n🙅‍♀️ fetching new volunteers failed! (${res.status}`)
-          return res.json()
-        })
-        .then((volunteersPage) => {
-          this.volunteersPage = volunteersPage
-        })
-        .finally(() => (this.fetching = false))
+      this.fetching = false
     },
     async fetchSortedVolunteers(sortBy) {
+      // If there's no token, something went wrong
+      if (!this.token) throw Error('VoloDB-ERROR\n🙅‍♀️ ups! not logged in.')
+
       this.fetching = true
+
       // call function for sortOrder
       this.defineSortOrder(sortBy)
-      await fetch(
-        `${import.meta.env.VITE_BASE_URL}/volunteers?&sortField=${sortBy}&sortOrder=${this.sortOrder}`,
-        {
-          method: 'GET',
-          headers: {
-            authorization: `Bearer ${this.token}`
-          }
-        }
+
+      this.volunteersPage = await vdbFetchData(
+        `volunteers?&sortField=${sortBy}&sortOrder=${this.sortOrder}`,
+        'GET',
+        this.token
       )
-        .then((res) => {
-          if (!res.ok) {
-            throw Error(`ERROR:${res.status}`)
-          }
-          return res.json()
-        })
-        .then((data) => {
-          this.volunteersPage = data
-        })
-        .finally(() => (this.fetching = false))
+      this.fetching = false
     },
     defineSortOrder(sortProperty) {
       if (sortProperty === this.activeSortProperty) {
---
+++ src/utils/api.js
@@ -0,0 +1,26 @@
+const baseUrl = import.meta.env.VITE_BASE_URL
+
+export const vdbFetchData = async (subdirectory, method, token, data) => {
+  // If there's no token, something went wrong
+  if (!token) throw Error('VoloDB-ERROR\n🙅‍♀️ ups! No token provided or not logged in.')
+
+  // If first character is a slash -> delete it
+  if (String(subdirectory).charAt(0) === '/') subdirectory = String(subdirectory).substring(1)
+
+  // Fetch Data from the Backend
+  return fetch(`${baseUrl}/${subdirectory}`, {
+    method,
+    headers: {
+      'content-type': 'application/json',
+      authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(data)
+  })
+    .then((res) => {
+      if (!res.ok) throw Error(`VoloDB-ERROR\n🙅‍♀️ fetching failed! (${res.status}`)
+      return res.json()
+    })
+    .then((data) => {
+      return data
+    })
+}
