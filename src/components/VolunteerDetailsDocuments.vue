041e2c2b49cf411e99bd719bbda53e442ddbb03e
Ari <arisenger0@gmail.com>
2024-06-05 14:17:14 +0200

    add:document component

---
+++ src/components/VolunteerDetailsDocuments.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+<table>
+    <thead>
+        <tr>
+            <td></td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <details>
+                <summary>
+                    
+                </summary>
+            </details>
+        </tr>
+    </tbody>
+</table>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
