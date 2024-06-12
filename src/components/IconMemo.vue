e9c3c35f66676503e1b9a2f0c178e4570ce730a0
Ari <arisenger0@gmail.com>
2024-06-12 14:40:34 +0200

    add: Icons

---
+++ src/components/IconMemo.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+<svg class="inline" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g opacity="0.5">
+<path d="M3.5 14L11.9688 14C12.9121 14 14 13.0192 14 11.8102L14 3.18977C14 1.97972 12.9121 1 11.9688 1L7.5 0.999999L3.03125 0.999999C2.08794 0.999999 1 1.97971 1 3.18977L1 11.5M3.5 14L1 11.5M3.5 14L3.5 11.5L1 11.5" stroke="#8C97AF" stroke-width="1.5"/>
+<path d="M4 4.5H11" stroke="#8C97AF" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M4 7.5H11" stroke="#8C97AF" stroke-width="1.5" stroke-linecap="round"/>
+<path d="M6 10.5H11" stroke="#8C97AF" stroke-width="1.5" stroke-linecap="round"/>
+</g>
+</svg>
+
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
