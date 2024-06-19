26c9a93d56ffc5b292170d8c8b7428de61b8d6e3
Jessica Schubert <jessica8schubert@gmail.com>
2024-06-19 13:07:14 +0200

    integrate user avatar to UserButton

--- src/components/UserButton.vue
+++ src/components/UserButton.vue
@@ -1,12 +1,30 @@
 <template>
-  <button rounded-e-full>
-    <!-- For User-Avatar use this path: VITE_BASE_URL + "/files" + user.avatar ??? -->
-    <!-- <img :src="VITE_BASE_URL + '/files' + user.avatar" /> -->
+  <button class="w-full flex justify-center items-center">
+    <img
+      v-if="userStore.user"
+      :src="baseUrl + '/files/' + userStore.user.avatar"
+      alt="Benutzer-Avatar"
+      class="aspect-square w-[39px] object-cover rounded-full"
+    />
   </button>
 </template>
 
+<!-- class="border-[3px] border-vologray-100 rounded-e-full" -->
+
 <script>
+import { useUserStore } from '@/stores/UserStore.js'
+
 export default {
-  name: 'UserButton'
+  name: 'UserButton',
+  components: {},
+  setup() {
+    const userStore = useUserStore()
+    const baseUrl = import.meta.env.VITE_BASE_URL
+
+    return {
+      userStore,
+      baseUrl
+    }
+  }
 }
 </script>
