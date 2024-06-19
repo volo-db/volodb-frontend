ebe2dd53f1a0283d2b15e37d0a71ce57be18f72b
Jessica Schubert <jessica8schubert@gmail.com>
2024-06-19 13:25:26 +0200

    add logout function

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -3,9 +3,26 @@
     <div class="flex justify-center items-center aspect-square">
       <img :src="smallVolodbLogo" alt="volodb-logo" />
     </div>
-    <div>
-      <slot name="nav-icon"></slot>
-    </div>
+    <NavBarButton type="login" v-if="!userStore.loggedIn" />
+    <NavBarButton type="dashboard" v-if="false" />
+    <NavBarButton
+      type="volunteers"
+      @click="goToVolunteersView()"
+      :active="$route.name == 'VolunteersView'"
+      v-if="userStore.loggedIn"
+    />
+    <NavBarButton
+      type="projects"
+      @click="goToProjectsView()"
+      :active="$route.name == 'ProjectsView'"
+      v-if="userStore.loggedIn"
+    />
+    <NavBarButton type="seminars" v-if="false" />
+    <div class="mt-auto"></div>
+    <NavBarButton type="mail" v-if="false" />
+    <NavBarButton type="settings" v-if="false" />
+    <UserButton v-if="userStore.loggedIn" />
+    <NavBarButton type="logout" @click="userStore.logout()" v-if="userStore.loggedIn" />
   </div>
 </template>
 
