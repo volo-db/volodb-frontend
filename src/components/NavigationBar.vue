63e58964650bd7f3e81d0f8323a9bcf901961c07
Jessica Schubert <jessica8schubert@gmail.com>
2024-06-19 12:53:41 +0200

    add NavBar styling and UserButton with avatar as component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,36 +1,46 @@
 <template>
-  <div class="w-[70px] bg-white">
+  <div class="w-[70px] bg-white flex flex-col">
     <div class="flex justify-center items-center aspect-square">
       <img :src="smallVolodbLogo" alt="volodb-logo" />
     </div>
-    <div>
-      <NavBarButton type="login" v-if="false" />
-      <NavBarButton type="dashboard" v-if="false" />
-      <NavBarButton
-        type="volunteers"
-        @click="goToVolunteersView()"
-        :active="$route.name == 'VolunteersView'"
-      />
-      <NavBarButton
-        type="projects"
-        @click="goToProjectsView()"
-        :active="$route.name == 'ProjectsView'"
-      />
-      <NavBarButton type="seminars" v-if="false" />
-      <NavBarButton type="mail" v-if="false" />
-      <NavBarButton type="settings" v-if="false" />
-      <NavBarButton type="user-button" />
-      <NavBarButton type="logout" v-if="false" />
-    </div>
+    <NavBarButton type="login" v-if="!userStore.loggedIn" />
+    <NavBarButton type="dashboard" v-if="false" />
+    <NavBarButton
+      type="volunteers"
+      @click="goToVolunteersView()"
+      :active="$route.name == 'VolunteersView'"
+      v-if="userStore.loggedIn"
+    />
+    <NavBarButton
+      type="projects"
+      @click="goToProjectsView()"
+      :active="$route.name == 'ProjectsView'"
+      v-if="userStore.loggedIn"
+    />
+    <NavBarButton type="seminars" v-if="false" />
+    <div class="mt-auto"></div>
+    <NavBarButton type="mail" v-if="false" />
+    <NavBarButton type="settings" v-if="false" />
+    <UserButton v-if="userStore.loggedIn" />
+    <NavBarButton type="logout" @click="userStore.logout()" v-if="userStore.loggedIn" />
   </div>
 </template>
 
 <script>
 import NavBarButton from '@/components/NavBarButton.vue'
 import smallVolodbLogo from '@/assets/logo-volodb-small.svg'
+import UserButton from '@/components/UserButton.vue'
+import { useUserStore } from '@/stores/UserStore.js'
 
 export default {
-  components: { NavBarButton },
+  components: { NavBarButton, UserButton },
+  setup() {
+    const userStore = useUserStore()
+
+    return {
+      userStore
+    }
+  },
   data() {
     return {
       smallVolodbLogo
