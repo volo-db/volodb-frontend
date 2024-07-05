eb4b4fd21280e31ab54f9d7291e41ad85b790ff6
Fabian Urner <fabian.urner+gh@gmail.com>
2024-07-05 13:15:08 +0200

    fix: delete unnecessary propertys and setup in component

--- src/components/VolunteerDetailOverviewAvatar.vue
+++ src/components/VolunteerDetailOverviewAvatar.vue
@@ -1,46 +1,27 @@
 <template>
   <div class="relative border-[3px] border-green-300 rounded-full p-2 group">
+    <img
+      class="aspect-square w-[110px] object-cover rounded-full transition group-hover:blur-sm"
+      :src="src"
+      :alt="alt"
+    />
     <div>
-      <img
-        class="aspect-square w-[110px] object-cover rounded-full transition group-hover:blur-sm"
-        :src="src"
-        :alt="alt"
-      />
-      <div>
-        <form>
-          <label
-            class="cursor-pointer absolute top-1/2 left-1/2 p-2 bg-white rounded-full shadow-md transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition"
-            for="edit"
-            ><IconPenEdit
-          /></label>
-          <input
-            id="edit"
-            type="file"
-            hidden
-            @change="$emit('fileSelected', $event)"
-            name="avatar"
-          />
-        </form>
-      </div>
+      <form>
+        <label
+          class="cursor-pointer absolute top-1/2 left-1/2 p-2 bg-white rounded-full shadow-md transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition"
+          for="edit"
+          ><IconPenEdit
+        /></label>
+        <input id="edit" type="file" hidden @change="$emit('fileSelected', $event)" name="avatar" />
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-import { useVolunteerStore } from '@/stores/VolunteerStore.js'
 import IconPenEdit from '@/components/IconPenEdit.vue'
 
 export default {
-  setup() {
-    const volunteerStore = useVolunteerStore()
-    const baseUrl = import.meta.env.VITE_BASE_URL
-
-    return {
-      volunteerStore,
-      baseUrl
-    }
-  },
-
   components: { IconPenEdit },
   props: ['src', 'alt']
 }
