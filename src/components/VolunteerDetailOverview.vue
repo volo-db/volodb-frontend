d05bb2547139e7ef3619d8f969e3b43b6e8d8a9e
Fabian Urner <fabian.urner+gh@gmail.com>
2024-07-05 11:52:47 +0200

    fix: delete unnecessary imports

--- src/components/VolunteerDetailOverview.vue
+++ src/components/VolunteerDetailOverview.vue
@@ -51,9 +51,6 @@
 </template>
 <script>
 import { useVolunteerStore } from '@/stores/VolunteerStore.js'
-import IconMail from '@/components/IconMail.vue'
-import IconPhone from '@/components/IconPhone.vue'
-import IconMessenger from '@/components/IconMessenger.vue'
 import VolunteerDetailOverviewAddresses from './VolunteerDetailOverviewAddresses.vue'
 import VolunteerDetailOverviewContact from './VolunteerDetailOverviewContact.vue'
 
