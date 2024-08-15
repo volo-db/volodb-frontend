f163130f162ea8356412c2aed904ba92049c46ff
Fabian Urner <fabian.urner+gh@gmail.com>
2024-08-15 13:10:41 +0200

    fix: transparent background for hover-effect

--- src/components/ButtonStandard.vue
+++ src/components/ButtonStandard.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- button standard is blue, for gray version use prop 'gray' -->
   <button
-    class="border-2 border-voloblue-200 rounded-lg text-white text-sm font-medium bg-voloblue-200 px-6 h-11 hover:text-voloblue-200 hover:bg-white hover:border-voloblue-200"
+    class="border-2 border-voloblue-200 rounded-lg text-white text-sm font-medium bg-voloblue-200 px-6 h-11 hover:text-voloblue-200 hover:bg-transparent hover:border-voloblue-200"
     :class="{
       'border-vologray-800': gray,
       'bg-vologray-800': gray,
-      'hover:bg-white': gray,
+      'hover:bg-transparent': gray,
       'hover:text-vologray-800': gray,
       'hover:border-vologray-800': gray
     }"
